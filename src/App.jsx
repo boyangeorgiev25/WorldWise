8c9c346be157dddda01f575f70dcdb1b160448ba
boyan <106580794+bobyTopG@users.noreply.github.com>
2025-07-28 22:56:01 +0300

    Fix import case sensitivity for Homepage component and add Docker configuration files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
-import HomePage from "./pages/HomePage";
+import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
