d1ecc4a3874fd98f75eb16a27fa46361af0b43a9
boyan <106580794+bobyTopG@users.noreply.github.com>
2025-08-04 10:41:02 +0300

    feat: integrate json-server for city data and implement CityList component
    - Added json-server as a dependency and configured it to serve city data.
    - Updated App component to fetch city data from the json-server API.
    - Created CityList component to display a list of cities with loading and empty states.
    - Added CityItem component to render individual city details.
    - Updated routing in App to include city listing.

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,31 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:3000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        setIsLoading(false);
+      } catch {
+        alert("There was an error loading data ....");
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -13,8 +36,14 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities || cities.length === 0)
+    return <p className={styles.empty}>No cities available.</p>;
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <li key={city.id} className={styles.cityItem}>
+          <CityItem city={city} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
