ceb2f84b8951119decce63cc80de4cb8521bc67c
Boyan Georgiev <106580794+boyangeorgiev25@users.noreply.github.com>
2025-08-10 21:44:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { CitiesProvider } from "./Context/CitiesContext.jsx";
-import { AuthProvider } from "./Context/AuthContext.jsx";
+import { CitiesProvider } from "./context/CitiesContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
